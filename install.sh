b5c0b106f782af39f23a3fbbdd43c20425f1a82a
kenranunderscore <johb.maier@gmail.com>
2018-03-25 14:12:23 +0200

    Use a loop

--- install.sh
+++ install.sh
@@ -1,6 +1,17 @@
 #!/usr/bin/env bash
 
 dotfile_dir=~/dotfiles
+backup_dir=~/dotfiles_backup
+targets=".zshrc .Xresources"
 
-ln -s $dotfile_dir/.Xresources ~/.Xresources
-ln -s $dotfile_dir/.zshrc ~/.zshrc
+echo "Creating $backup_dir..."
+mkdir -p $backup_dir
+echo "Changing to $dotfile_dir..."
+cd $dotfile_dir
+
+echo "Backing up and creating symlinks..."
+for target in $targets; do
+    mv ~/$target $backup_dir
+    ln -s $dotfile_dir/$target ~/$target
+done
+
