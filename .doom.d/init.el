a09d7471750786d8acaae606920b0d526b600844
kenranunderscore <johb.maier@gmail.com>
2018-12-11 20:06:43 +0100

    Use helm's fuzzy search

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -15,7 +15,8 @@
 
        :completion
        company           ; the ultimate code completion backend
-       helm              ; the *other* search engine for love and life
+       (helm              ; the *other* search engine for love and life
+        +fuzzy)
        ;;ido              ; the other *other* search engine...
        ;;ivy              ; a search engine for love and life
 
