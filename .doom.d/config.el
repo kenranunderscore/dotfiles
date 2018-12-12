dc9e59367d957f548e245d67a9fa8561edcc521e
kenranunderscore <johb.maier@gmail.com>
2018-12-12 20:22:35 +0100

    Change some keybindings to what I'm used to

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,3 +1,27 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
 (setq doom-font (font-spec :family "Source Code Pro" :size 13))
+
+(add-to-list 'default-frame-alist '(fullscreen . maximized))
+
+(setq doom-localleader-key ",")
+
+(map!
+ ;; text scaling
+ "M-+"     (λ! (text-scale-set 0))
+ "M-="     #'text-scale-increase
+ "M--"     #'text-scale-decrease
+
+ (:leader
+   ;; I'm used to doing SPC SPC for M-x
+   :desc "M-x" :nv "SPC" #'helm-M-x
+
+   ;; projectile
+   (:desc "project" :prefix "p"
+     :desc "Find file in project" :nv "f" #'projectile-find-file
+     :desc "Kill project buffers" :nv "k" #'projectile-kill-buffers)
+
+   ;; git / magit
+   (:desc "git" :prefix "g"
+     :desc "Git status"     :nv "s" #'magit-status
+     :desc "Git stage hunk" :n  "g" #'git-gutter:stage-hunk)))
