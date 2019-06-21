a087e168c70aacccabaf0aa1ab6704103aa67875
kenranunderscore <johannes.maier@active-group.de>
2019-06-21 14:58:53 +0200

    Configure Haskell mode bindings and add themes

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -1,6 +1,9 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Input" :size 16))
+(setq doom-font (font-spec :family "Input" :size 15))
+(setq doom-theme 'doom-one)
+
+(setq confirm-kill-emacs nil)
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 
@@ -37,4 +40,9 @@
    ;; git / magit
    (:desc "git" :prefix "g"
      :desc "Git status"     :nv "s" #'magit-status
-     :desc "Git stage hunk" :n  "g" #'git-gutter:stage-hunk)))
+     :desc "Git stage hunk" :n  "g" #'git-gutter:stage-hunk))
+
+ (:after haskell-mode
+   :map haskell-mode-map
+   :localleader
+   :n "F" #'haskell-mode-stylish-buffer))
--- .doom.d/packages.el
+++ .doom.d/packages.el
@@ -2,6 +2,8 @@
 ;;; ~/.doom.d/packages.el
 
 ;;; Examples:
-(package! 'kaolin-themes)
+(package! kaolin-themes)
+(package! base16-theme)
+(package! moe-theme)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
