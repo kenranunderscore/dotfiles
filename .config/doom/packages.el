62a040a8fced3543459c4ba0d56084ff158a3028
Johannes Maier <johannes.maier@active-group.de>
2020-03-03 07:44:53 +0100

    Stop using the doom themes fork for now

--- .config/doom/packages.el
+++ .config/doom/packages.el
@@ -6,10 +6,5 @@
 ;; (package! another-package :recipe (:host github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 
-;; Use a fork of emacs-doom-themes to be able to develop
-;; custom themes myself
-(package! emacs-doom-themes
-  :recipe (:host github :repo "kenranunderscore/emacs-doom-themes" :branch "master"))
-
 (package! glsl-mode)
 (package! mu4e-alert)
