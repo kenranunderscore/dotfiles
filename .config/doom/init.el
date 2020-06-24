73c0ce7e82a7902093794ee8548adc1b0ba76f12
Johannes Maier <johb.maier@gmail.com>
2020-06-25 01:02:59 +0200

    Use racket-mode

--- .config/doom/init.el
+++ .config/doom/init.el
@@ -145,7 +145,7 @@
        purescript        ; javascript, but functional
        ;;python            ; beautiful is better than ugly
        ;;qt                ; the 'cutest' gui framework ever
-       ;;racket            ; a DSL for DSLs
+       racket            ; a DSL for DSLs
        ;;rest              ; Emacs as a REST client
        ;;ruby              ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        ;;(rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
