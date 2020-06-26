ab554fb33b8b3e57fe434227661998c8f71d0712
Johannes Maier <johb.maier@gmail.com>
2020-06-26 22:29:06 +0200

    Home manager: sign git commits by default

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -22,6 +22,8 @@ in
       then "johannes.maier@active-group.de"
       else "johb.maier@gmail.com";
     ignores = [];
+    signing.signByDefault = true;
+    signing.key = "0BAD1500D7D4282C433BC0BC9AC78C1A48681583";
     extraConfig = {
       pull.rebase = "false";
       core.editor = "vim";
