001507396e8d9b483dc721d5e2951fbabfb72600
Johannes Maier <johb.maier@gmail.com>
2020-06-29 23:24:32 +0200

    Move out git config

--- home.nix
+++ home.nix
@@ -15,25 +15,7 @@ in {
   programs = {
     home-manager.enable = true;
 
-    git = {
-      package = pkgs.gitAndTools.gitFull;
-      enable = true;
-      userName = "Johannes Maier";
-      userEmail = if isDarwin then
-        "johannes.maier@active-group.de"
-      else
-        "johb.maier@gmail.com";
-      ignores = [ "*.swp" ];
-      signing.signByDefault = true;
-      signing.key = if isDarwin then
-        "0x4DC80C3B727DC1EE"
-      else
-        "0BAD1500D7D4282C433BC0BC9AC78C1A48681583";
-      extraConfig = {
-        pull.rebase = "false";
-        core.editor = "vim";
-      };
-    };
+    git = import ./config/git.nix pkgs isDarwin;
 
     kitty = {
       enable = true;
@@ -224,9 +206,7 @@ in {
       addXterm24bitTerminfo =
         let tic = if isDarwin then "/usr/bin/tic" else "tic";
         in dagEntryAfter [ "writeBoundary" ] ''
-          $DRY_RUN_CMD ${tic} -x -o ~/.terminfo ${
-            ./config/xterm-24bit.terminfo
-          }
+          $DRY_RUN_CMD ${tic} -x -o ~/.terminfo ${./config/xterm-24bit.terminfo}
         '';
 
       addSshKey = let privateKeyPath = osPrivatePath + "/id_rsa";
