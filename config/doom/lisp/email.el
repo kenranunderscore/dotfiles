15990242a5d4c94b392880201f6fa07c43e1a88c
Johannes Maier <johb.maier@gmail.com>
2020-08-05 22:22:38 +0200

    Begin working on better gmail integration

--- config/doom/lisp/email.el
+++ config/doom/lisp/email.el
@@ -16,7 +16,10 @@
                               "Hechinger Str. 12/1\n"
                               "72072 Tübingen\n"
                               "Registergericht: Amtsgericht Stuttgart, HRB 224404\n"
-                              "Geschäftsführer: Dr. Michael Sperber"))))
+                              "Geschäftsführer: Dr. Michael Sperber"))
+   (mu4e-sent-messages-behavior . 'sent)
+   (mu4e-index-cleanup . t)
+   (mu4e-index-lazy-check . nil)))
 
 (set-email-account!
  "private"
@@ -25,16 +28,21 @@
    (mu4e-compose-signature . nil)
    (mu4e-drafts-folder     . "/private/[Google Mail].Drafts")
    (mu4e-trash-folder      . "/private/[Google Mail].Trash")
-   (mu4e-refile-folder     . "/private/[Google Mail].All Mail"))
+   (mu4e-refile-folder     . "/private/[Google Mail].All Mail")
+   (mu4e-sent-messages-behavior . 'delete)
+   (mu4e-index-cleanup . nil)
+   (mu4e-index-lazy-check . t))
  t)
 
 (after! mu4e
   ;; Make accidentally quitting mu4e harder.
   (setq mu4e-confirm-quit t)
+
   ;; Ask once initially, and when composing all
   ;; should work. In any case, ask if it doesn't.
   (setq mu4e-context-policy 'ask-if-none)
   (setq mu4e-compose-context-policy 'ask-if-none)
+
   ;; Use msmtp to send mails instead of smtpmail.
   ;; The following options are all necessary to respect
   ;; the account chosen by mu4e.
@@ -43,4 +51,10 @@
   (setq message-send-mail-function 'sendmail-send-it)
   (setq mail-specify-envelope-from t)
   (setq message-sendmail-envelope-from 'header)
-  (setq mail-envelope-from 'header))
+  (setq mail-envelope-from 'header)
+
+  ;; Try to work around some gmail-specific issues by
+  ;; refining doom's +gmail module flag code.
+  (add-hook! 'mu4e-mark-execute-pre-hook
+    (defun +mu4e-gmail-fix-flags-h (mark msg)
+      (print msg))))
