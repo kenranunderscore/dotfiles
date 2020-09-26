e7b58158187243ce7499640e98a56cacf25548e9
Johannes Maier <johb.maier@gmail.com>
2020-09-26 20:57:15 +0200

    Use JetBrains Mono in emacs on NixOS
    It doesn't look as good on macOS for me though due to the way it renders
    fonts more thickly there.

--- config/doom/lisp/linux.el
+++ config/doom/lisp/linux.el
@@ -1,4 +1,4 @@
 ;;; ~/.config/doom/lisp/linux.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "DejaVu Sans Mono" :size 14))
+(setq doom-font (font-spec :family "JetBrains Mono" :size 15))
 (setq doom-theme 'doom-material)
