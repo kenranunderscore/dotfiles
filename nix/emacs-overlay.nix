d68be2d1e9a9e16ca3d42775a6d191a9e3f23f8e
Johannes Maier <johb.maier@gmail.com>
2020-12-12 14:46:58 +0100

    Upgrade gccemacs

--- nix/emacs-overlay.nix
+++ nix/emacs-overlay.nix
@@ -1,5 +1,5 @@
 # This overlay enables us to use unstable, HEAD, or even gcc emacs.
-let rev = "1b1fc523f4286a28193f70bfdbf6c2098e3e8967";
+let rev = "16fc85a9522705b7ff1e479cf06975d99ebb7b55";
 in (import (builtins.fetchTarball {
   url = "https://github.com/nix-community/emacs-overlay/archive/${rev}.tar.gz";
 }))
