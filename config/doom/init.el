39f8e6e520a968d5693dad0f9b7f088705c289e7
Johannes Maier <johb.maier@gmail.com>
2020-12-16 14:06:54 +0100

    Enable Common Lisp module

--- config/doom/init.el
+++ config/doom/init.el
@@ -107,7 +107,7 @@
        ;;assembly          ; assembly for fun or debugging
        ;;cc                ; C/C++/Obj-C madness
        clojure           ; java with a lisp
-       ;;common-lisp       ; if you've seen one lisp, you've seen them all
+       common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
