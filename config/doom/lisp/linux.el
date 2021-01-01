298aa11b5286ca4082714cb0a31f196e5d739016
Johannes Maier <johb.maier@gmail.com>
2021-01-01 21:18:55 +0100

    Switch to Cascadia Mono

--- config/doom/lisp/linux.el
+++ config/doom/lisp/linux.el
@@ -1,4 +1,4 @@
 ;;; ~/.config/doom/lisp/linux.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "JetBrains Mono" :size 14))
+(setq doom-font (font-spec :family "Cascadia Mono" :weight 'normal :size 16))
 (setq doom-theme 'doom-material)
