e120c42c0cd2ee92f05b07183b103a0538678dee
Johannes Maier <johb.maier@gmail.com>
2021-01-05 09:52:24 +0100

    Use doom-themes on main

--- config/doom/packages.el
+++ config/doom/packages.el
@@ -1,10 +1,8 @@
 ;; -*- no-byte-compile: t; -*-
 ;;; .doom.d/packages.el
 
-;;; Examples:
-;; (package! some-package)
-;; (package! another-package :recipe (:host github :repo "username/repo"))
-;; (package! builtin-package :disable t)
+;; We want to be using fresh doom themes.
+(unpin! doom-themes)
 
 (package! glsl-mode)
 (package! mu4e-alert)
