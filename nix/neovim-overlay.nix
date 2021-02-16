5842ccd29d6c17b6bbe5bd5d526977b3ba1f917d
Johannes Maier <johb.maier@gmail.com>
2021-02-16 20:43:56 +0100

    Update neovim

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -1,5 +1,6 @@
 # This overlay enables us to use neovim HEAD, or in general recent commits.
-let rev = "350d5517833d0906f8e90bb882db45460ac5b830";
+let rev = "cf6e8a71c34b6dea741f920f9e6fe4b5328fb2b5";
 in (import (builtins.fetchTarball {
-  url = "https://github.com/nix-community/neovim-nightly-overlay/archive/${rev}.tar.gz";
+  url =
+    "https://github.com/nix-community/neovim-nightly-overlay/archive/${rev}.tar.gz";
 }))
