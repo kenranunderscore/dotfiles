254b0626b35ff7f01b401002f34d1a0ad7b75ac4
Johannes Maier <johb.maier@gmail.com>
2021-02-27 23:22:31 +0100

    Adapt to new single-screen setup

--- config/doom/lisp/linux.el
+++ config/doom/lisp/linux.el
@@ -1,4 +1,4 @@
 ;;; ~/.config/doom/lisp/linux.el -*- lexical-binding: t; -*-
 
-(setq doom-font (font-spec :family "Cascadia Mono" :weight 'normal :size 16))
+(setq doom-font (font-spec :family "Cascadia Mono" :weight 'normal :size 18))
 (setq doom-theme 'doom-material)
