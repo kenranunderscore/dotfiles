f8b2e2bf66a5230c414c74936a8ddbee663975f3
Johannes Maier <johb.maier@gmail.com>
2021-03-06 09:53:33 +0100

    Fix bash on non-NixOS systems

--- modules/shell/bash.nix
+++ modules/shell/bash.nix
@@ -9,6 +9,8 @@ in {
     programs.bash = {
       enable = true;
       historyIgnore = [ "ls" "cd" "exit" ];
+      initExtra = "source $HOME/.nix-profile/etc/profile.d/nix.sh";
+      profileExtra = "source $HOME/.nix-profile/etc/profile.d/nix.sh";
       shellAliases = import ./shell-aliases.nix;
     };
   };
