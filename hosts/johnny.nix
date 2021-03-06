bf92686ea96ad3c13d4b617b207c0d04933ec3a8
Johannes Maier <johb.maier@gmail.com>
2021-03-06 23:47:42 +0100

    Add macOS host

---
+++ hosts/johnny.nix
@@ -0,0 +1,36 @@
+{ config, lib, pkgs, ... }:
+
+let dag = import <home-manager/modules/lib/dag.nix> { inherit lib; };
+in {
+  imports = [ ./base.nix ../modules ];
+
+  hosts.base.username = "maier";
+
+  modules = {
+    email = {
+      certificatesFile = "/usr/local/etc/openssl/cert.pem";
+      primaryAccount = "ag";
+    };
+    programs = {
+      kitty = {
+        useLoginShell = true;
+        fontSize = "17.0";
+      };
+    };
+    shell = {
+      git = {
+        email = "johannes.maier@active-group.de";
+        gpgKey = "0x4DC80C3B727DC1EE";
+      };
+    };
+  };
+
+  home = {
+    activation = {
+      symlinkMacOSApps = let
+        action =
+          "$DRY_RUN_CMD ln -snf $HOME/.nix-profile/Applications/*.app ~/Applications/";
+      in dag.dagEntryAfter [ "writeBoundary" ] action;
+    };
+  };
+}
