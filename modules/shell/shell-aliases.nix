67ed82fe5d71e2619d826836c0cf8f81781cb49b
Johannes Maier <johannes.maier@active-group.de>
2021-04-06 08:27:50 +0200

    Add some shell aliases

--- modules/shell/shell-aliases.nix
+++ modules/shell/shell-aliases.nix
@@ -3,11 +3,14 @@
   e = "emacsclient --alternate-editor '' --create-frame --no-wait";
   # kill the emacs daemon
   ekill = "emacsclient --eval '(kill-emacs)'";
+  # try using emacs/magit as replacement for CLI git
+  eg = "emacsclient --eval '(magit-status)'";
 
   # git aliases
   g = "git";
   ga = "git add";
   gc = "git commit -v";
+  gca = "git commit -v --amend";
   gcm = "git commit -v -m";
   gd = "git diff";
   gds = "git diff --staged";
@@ -19,8 +22,10 @@
   gp = "git push";
   grb = "git rebase";
   grs = "git restore";
+  grss = "git restore --staged";
   gs = "git status --short";
   gst = "git status";
+  gsw = "git switch";
   gupa = "git pull --rebase --autostash";
 
   # misc
