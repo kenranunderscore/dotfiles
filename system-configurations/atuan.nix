ca41ba02f56df5b72b82f54ed11547eed9d78aff
Johannes Maier <johb.maier@gmail.com>
2021-04-09 18:51:27 +0200

    Enable docker

--- system-configurations/atuan.nix
+++ system-configurations/atuan.nix
@@ -91,6 +91,8 @@
 
   environment.systemPackages = with pkgs; [ ];
 
+  virtualisation.docker.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
