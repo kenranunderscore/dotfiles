55bd174eaa16ac7ae675e5bb5494cffd4ea9dfa1
Johannes Maier <johb.maier@gmail.com>
2021-05-01 10:17:55 +0200

    Zap byte-compilation of my config
    It's not yielded any startup time improvements so far, plus it leads
    to a lot of warnings.

--- modules/programs/emacs/.emacs.d/init.el
+++ modules/programs/emacs/.emacs.d/init.el
@@ -3,13 +3,10 @@
 ;; This checks whether there already exists an up-to-date
 ;; byte-compiled configuration file, otherwise it is created before
 ;; loading it.
-;; TODO Measure startup time to see if byte-compiling is worth it
 ;; TODO Add a hook that does this on saving config.org
 (let* ((config (concat user-emacs-directory "config.org"))
-       (compiled-config (concat user-emacs-directory "config.el"))
-       (byte-compiled-config (concat compiled-config "c")))
-  (when (or (not (file-exists-p byte-compiled-config))
-	    (file-newer-than-file-p config byte-compiled-config))
-    (org-babel-tangle-file config compiled-config)
-    (byte-compile-file compiled-config))
-  (load-file byte-compiled-config))
+       (compiled-config (concat user-emacs-directory "config.el")))
+  (when (or (not (file-exists-p compiled-config))
+	    (file-newer-than-file-p config compiled-config))
+    (org-babel-tangle-file config compiled-config))
+  (load-file compiled-config))
