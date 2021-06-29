c17d3ef8e6f1c781106d7d534784fe6d7a3c9a9e
Johannes Maier <johannes.maier@active-group.de>
2021-06-29 07:56:43 +0200

    Join #nixos on libera

--- modules/programs/weechat/irc.conf.nix
+++ modules/programs/weechat/irc.conf.nix
@@ -191,7 +191,7 @@ certPath:
   liberachat.usermode
   liberachat.command
   liberachat.command_delay
-  liberachat.autojoin = "#racket,#linux,#emacs,#org-mode,#haskell"
+  liberachat.autojoin = "#racket,#linux,#emacs,#org-mode,#haskell,#nixos"
   liberachat.autorejoin
   liberachat.autorejoin_delay
   liberachat.connection_timeout
