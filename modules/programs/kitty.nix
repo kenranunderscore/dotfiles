a22d6bbfd9e526e9627dcf8659d76e7103984b8b
Johannes Maier <johb.maier@gmail.com>
2021-07-12 21:43:23 +0200

    Update kitty color scheme

--- modules/programs/kitty.nix
+++ modules/programs/kitty.nix
@@ -43,43 +43,27 @@ in {
         initial_window_height = 520;
         enable_audio_bell = false;
         # Color scheme
-        background = "#162126";
-        foreground = "#a6b5b5";
-        selection_background = "#a6b5b5";
-        selection_foreground = "#162126";
-        url_color = "#878573";
-        cursor = "#929181";
-        active_border_color = "#6c6b5a";
-        inactive_border_color = "#302f27";
-        active_tab_background = "#22221b";
-        active_tab_foreground = "#929181";
-        inactive_tab_background = "#302f27";
-        inactive_tab_foreground = "#878573";
-        tab_bar_background = "#302f27";
-        # black
-        color0 = "#070b0d";
-        color8 = "#0f161a";
-        # red
-        color1 = "#f72c25";
-        color9 = "#f72c25";
-        # green
-        color2 = "#6bde38";
-        color10 = "#6bde38";
-        # yellow
-        color3 = "#fcca46";
-        color11 = "#fcca46";
-        # blue
-        color4 = "#1b98e0";
-        color12 = "#1b98e0";
-        # magenta
-        color5 = "#f374ae";
-        color13 = "#f374ae";
-        # cyan
-        color6 = "#46d9ff";
-        color14 = "#46d9ff";
-        # white
-        color7 = "#a6b5b5";
-        color15 = "#a6b5b5";
+        background = "#001014";
+        foreground = "#405555";
+        cursor = "#49fcd5";
+        selection_background = "#415554";
+        color0 = "#012026";
+        color8 = "#374350";
+        color1 = "#b12f2c";
+        color9 = "#ff4242";
+        color2 = "#00a940";
+        color10 = "#2aea5e";
+        color3 = "#5d8aa9";
+        color11 = "#8dd3fd";
+        color4 = "#449985";
+        color12 = "#61d4b9";
+        color5 = "#00599c";
+        color13 = "#1298ff";
+        color6 = "#5c7e19";
+        color14 = "#98cf28";
+        color7 = "#405554";
+        color15 = "#58fad6";
+        selection_foreground = "#001014";
       };
     };
   };
