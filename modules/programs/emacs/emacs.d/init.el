f5e97f02ea657d4bdab28e30176789151df1cf46
Johannes Maier <johb.maier@gmail.com>
2021-07-17 18:43:34 +0200

    Fix evil-org startup hook

--- modules/programs/emacs/emacs.d/init.el
+++ modules/programs/emacs/emacs.d/init.el
@@ -238,7 +238,7 @@
 ;; well.
 (use-package evil-org
   :after (evil org)
-  :hook (org-mode . (lambda () evil-org-mode))
+  :hook (org-mode . evil-org-mode)
   :config
   (require 'evil-org-agenda)
   (evil-org-agenda-set-keys))
