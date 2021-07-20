c7f3b194cbed436546be411b53f78695357ad2d6
Johannes Maier <johannes.maier@active-group.de>
2021-07-20 22:24:54 +0200

    Update and cleanup fish modules

--- modules/shell/fish.nix
+++ modules/shell/fish.nix
@@ -27,16 +27,7 @@ in with lib; {
             owner = "danhper";
             repo = "fish-fastdir";
             rev = "4f616a6071f05ae89d5f72c20216f6f851e6ca1d";
-            sha256 = "1kr90b03pry7k74g80wnmmlw2d6pvp3637cifywqpkwdyizys6va";
-          };
-        }
-        {
-          name = "fish-async-prompt";
-          src = pkgs.fetchFromGitHub {
-            owner = "acomagu";
-            repo = "fish-async-prompt";
-            rev = "40f30a4048b81f03fa871942dcb1671ea0fe7a53";
-            sha256 = "19i59145lsjmidqlgk2dmvs3vg2m3zlz2rcms2kyyk1m3y63q8xi";
+            sha256 = "sha256:1kr90b03pry7k74g80wnmmlw2d6pvp3637cifywqpkwdyizys6va";
           };
         }
         {
@@ -45,7 +36,7 @@ in with lib; {
             owner = "jorgebucaran";
             repo = "autopair.fish";
             rev = "1222311994a0730e53d8e922a759eeda815fcb62";
-            sha256 = "0lxfy17r087q1lhaz5rivnklb74ky448llniagkz8fy393d8k9cp";
+            sha256 = "sha256:0lxfy17r087q1lhaz5rivnklb74ky448llniagkz8fy393d8k9cp";
           };
         }
       ];
