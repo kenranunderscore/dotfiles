860cf723a79d9b288d6343e619b9595245c388c3
Johannes Maier <johannes.maier@mailbox.org>
2021-09-12 11:44:39 +0200

    Prettify gnus
    Thanks Prot for still having your .gnus up :)

--- modules/programs/emacs/emacs.d/lisp/gnus-configuration.el
+++ modules/programs/emacs/emacs.d/lisp/gnus-configuration.el
@@ -26,7 +26,35 @@
           (nnimap "mailbox"
                   (nnimap-address "imap.mailbox.org")
                   (nnimap-server-port 993)
-                  (nnimap-stream ssl)))))
+                  (nnimap-stream ssl))))
+  ;; Visuals
+  (setq gnus-sum-thread-tree-indent "  ")
+  (setq gnus-sum-thread-tree-root "● ")
+  (setq gnus-sum-thread-tree-false-root "◯ ")
+  (setq gnus-sum-thread-tree-single-indent "◎ ")
+  (setq gnus-sum-thread-tree-vertical        "│")
+  (setq gnus-sum-thread-tree-leaf-with-other "├─► ")
+  (setq gnus-sum-thread-tree-single-leaf     "╰─► ")
+  (setq gnus-summary-line-format
+        (concat
+         "%0{%U%R%z%}"
+         "%3{│%}" "%1{%d%}" "%3{│%}" ;; date
+         "  "
+         "%4{%-20,20f%}" ;; name
+         "  "
+         "%3{│%}"
+         " "
+         "%1{%B%}"
+         "%s\n"))
+  (setq gnus-summary-display-arrow t))
+
+(use-package gnus-topic
+  :ensure nil
+  :after gnus
+  :hook
+  (gnus-group-mode . gnus-topic-mode)
+  :config
+  (setq gnus-topic-display-empty-topics t))
 
 ;; FIXME gcc: should it put sent mail into the group it came from?
 ;; Is that perhaps the default?
