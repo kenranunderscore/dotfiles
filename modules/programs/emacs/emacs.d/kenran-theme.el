a3f00a00296af8f75287cce132f09ee53012fd35
Johannes Maier <johannes.maier@mailbox.org>
2021-09-26 19:33:03 +0200

    Add first draft of custom theme

---
+++ modules/programs/emacs/emacs.d/kenran-theme.el
@@ -0,0 +1,31 @@
+(deftheme kenran
+  "Created 2021-09-26.")
+
+(let ((bg "#040404")
+      (fg "green3"))
+  (apply #'custom-theme-set-faces
+         `(kenran
+           (default ((t (:foreground ,fg :background ,bg))))
+           (minibuffer-prompt ((t (:weight semi-bold :foreground "#ff9800"))))
+           (highlight ((t (:foreground ,fg :background "gray12" :weight semi-bold))))
+           (region ((t (:background "blue4"))))
+           (font-lock-builtin-face ((t (:foreground "orange"))))
+           (font-lock-comment-face ((t (:slant italic :foreground "#707370"))))
+           (font-lock-constant-face ((t (:foreground "hot pink"))))
+           (font-lock-doc-face ((t (:slant italic :foreground "olivedrab4"))))
+           (font-lock-function-name-face ((t (:foreground "cyan2"))))
+           (font-lock-keyword-face ((t (:foreground "green3" :weight bold))))
+           (font-lock-preprocessor-face ((t (:inherit (font-lock-constant-face)))))
+           (font-lock-string-face ((t (:foreground "dark khaki"))))
+           (font-lock-type-face ((t (:foreground "yellow"))))
+           (font-lock-variable-name-face ((t (:foreground ,fg))))
+           (fringe ((t (:background ,bg))))
+           (warning ((t (:foreground "orange red" :weight regular))))
+           (mode-line ((t (:background "gray9" :box "#cccccc"))))
+           (mode-line-buffer-id ((t (:weight bold))))
+           (mode-line-emphasis ((t (:weight bold))))
+           (mode-line-inactive ((t (:box "#555555" :background ,bg :foreground "#707370"))))
+           (isearch ((t (foreground "green" :weight semi-bold :background "blue"))))
+           (lazy-highlight ((t (:foreground ,fg :background "blue3")))))))
+
+(provide-theme 'kenran)
