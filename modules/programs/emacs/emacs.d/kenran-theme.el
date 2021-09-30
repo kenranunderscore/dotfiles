546c9b22a4d4bc3b67af7f3c1966b0b0b4c56281
Johannes Maier <johannes.maier@mailbox.org>
2021-09-30 21:40:26 +0200

    Continue styling stuff

--- modules/programs/emacs/emacs.d/kenran-theme.el
+++ modules/programs/emacs/emacs.d/kenran-theme.el
@@ -14,8 +14,8 @@
             `((t ,@(cdr entry)))))
     `((default (:foreground ,fg :background ,bg))
       (minibuffer-prompt (:foreground ,light-olive))
-      (highlight (:foreground ,fg :background "gray12" :weight semi-bold))
-      (region (:background "blue4"))
+      (highlight (:foreground ,fg :background ,dark-blue))
+      (region (:background ,dark-blue))
       (font-lock-builtin-face (:foreground ,orange))
       (font-lock-comment-face (:slant italic :foreground ,comment))
       (font-lock-constant-face (:foreground ,pink))
@@ -25,11 +25,12 @@
       (font-lock-preprocessor-face (:inherit (font-lock-constant-face)))
       (font-lock-string-face ((t (:foreground ,string))))
       (font-lock-type-face (:foreground "olivedrab3"))
-      (font-lock-variable-name-face (:foreground ,fg))
+      ;; TODO This is not yet decided, maybe use semi-bold fg instead?
+      (font-lock-variable-name-face (:foreground "medium sea green"))
       (font-lock-warning-face (:slant italic :foreground "orange red"))
       (fringe (:background ,bg))
       (warning (:foreground "orange red" :weight regular))
-      (mode-line (:background ,bg :foreground ,light-olive :box "#cccccc"))
+      (mode-line (:background ,bg :foreground ,fg :box "#cccccc"))
       (mode-line-buffer-id (:weight bold))
       (mode-line-emphasis (:weight bold))
       (mode-line-inactive (:box "#555555" :background ,bg :foreground ,comment))
@@ -38,10 +39,10 @@
       (show-paren-match (:foreground ,bg :background ,fg))
 
       ;; orderless
-      (orderless-match-face-0 (:foreground ,light-olive))
-      (orderless-match-face-1 (:foreground ,cyan))
-      (orderless-match-face-2 (:foreground ,orange))
-      (orderless-match-face-3 (:foreground ,yellow))
+      (orderless-match-face-0 (:foreground ,pink))
+      (orderless-match-face-1 (:foreground ,light-olive))
+      (orderless-match-face-2 (:foreground ,yellow))
+      (orderless-match-face-3 (:foreground ,orange))
 
       ;; outline-*, and by extension org-level-*
       (outline-1 (:weight semi-bold :foreground ,fg))
@@ -56,7 +57,7 @@
       ;; company
       (company-tooltip (:background "gray10"))
       (company-tooltip-common (:foreground ,light-olive))
-      (company-tooltip-selection (:background "blue4" :weight bold))
+      (company-tooltip-selection (:background ,dark-blue :weight bold))
 
       ;; which-key
       (which-key-key-face (:foreground ,yellow))
@@ -77,18 +78,30 @@
       (org-todo (:foreground "orange red"))
       (org-done (:foreground ,fg))
       (org-headline-todo (:foreground "orange red"))
-      (org-headline-done (:foreground ,comment :strike-through t)))))
+      (org-headline-done (:foreground ,comment :strike-through t))
+
+      ;; magit
+      (magit-section-heading (:foreground ,orange :weight semi-bold))
+      (magit-section-highlight (:background ,dark-blue))
+      (magit-branch-local (:foreground ,yellow))
+      (magit-branch-remote (:foreground ,cyan))
+      (magit-diff-file-heading-highlight (:background ,dark-blue))
+      (magit-diff-context-highlight (:background "gray15" :foreground "gray65"))
+      (magit-diff-context (:foreground "gray40"))
+      (magit-diff-hunk-heading (:background "gray12" :foreground "gray70"))
+      (magit-diff-hunk-heading-highlight (:background "gray20" :foreground "gray80")))))
 
 ;; Set all the colors to their actual values.
 (let ((bg "#040404")
       (fg "green3")
-      (yellow "yellow3")
+      (yellow "gold2")
       (cyan "cyan3")
       (string "dark khaki")
       (pink "hot pink")
       (orange "#dc7612")
       (light-olive "olivedrab2")
-      (comment "#707370"))
+      (comment "#707370")
+      (dark-blue "blue4"))
   (apply #'custom-theme-set-faces
          (cons 'kenran (create-theme-colors))))
 
