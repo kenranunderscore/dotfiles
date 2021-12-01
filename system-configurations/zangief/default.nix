d45ebab7da376dd5d99dcdfd14531ccc565f63ba
Johannes Maier <johannes.maier@active-group.de>
2021-12-01 18:01:58 +0100

    Mount Ubuntu's filesystem in NixOS

--- system-configurations/zangief/default.nix
+++ system-configurations/zangief/default.nix
@@ -94,6 +94,11 @@
 
   virtualisation.docker.enable = true;
 
+  fileSystems."/ubuntu" = {
+    device = "/dev/sda2";
+    fsType = "ext4";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
