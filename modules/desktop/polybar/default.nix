1aaa61a470d5adc8cc430777d1a175b871317aa0
Johannes Maier <johannes.maier@mailbox.org>
2022-01-30 12:55:40 +0100

    Switch to polybar
    It's still very basic, but more easily extendable.

--- modules/desktop/polybar/default.nix
+++ modules/desktop/polybar/default.nix
@@ -5,13 +5,15 @@ in {
   options.modules.desktop.polybar = { enable = lib.mkEnableOption "polybar"; };
 
   config = lib.mkIf cfg.enable {
+    # Using services.polybar doesn't work for me.  It's probably due
+    # to using startx, so I'd have to start it manually.
     home.packages = let
       myPolybar = pkgs.polybar.override {
         alsaSupport = true;
         githubSupport = true;
         pulseSupport = true;
       };
-    in [ pkgs.polybar ];
+    in [ myPolybar ];
     xdg.configFile."polybar/config".source = ./config.ini;
   };
 }
