b233edf2645dff197ba9a65278377567142106d9
Johannes Maier <johannes.maier@mailbox.org>
2022-03-13 18:52:14 +0100

    picom: make nyxt opaque

--- modules/desktop/picom.nix
+++ modules/desktop/picom.nix
@@ -16,6 +16,7 @@ in {
         ''100:class_i = "rofi"''
         ''100:class_i = "DBeaver"''
         ''100:class_i = "emacs"''
+        ''100:class_i = "nyxt"''
         ''100:class_i = "VirtualBox Machine"''
         ''100:class_i = "VirtualBox Manager"''
       ];
