53575d3917944ee8593382e71327e66d5a33beb7
Johannes Maier <johannes.maier@active-group.de>
2022-03-18 06:45:55 +0100

    zsh: begin evaluation vs fish
    My old zsh config is out of date and no longer functioning.  I want to
    evaluate whether I can get a snappy, POSIX-compatible setup with
    zsh (again), but keeping the fish default features I like, mainly:
    
    - abbreviations (zsh-abbr)
    - nice history
    - ...

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -5,10 +5,31 @@ in {
   options.modules.shell.zsh.enable = lib.mkEnableOption "zsh";
 
   config = lib.mkIf cfg.enable {
-    home.file = {
-      ".zshrc".source = ./zshrc;
-      ".zshenv".source = ./zshenv;
+    # TODO(Johannes):
+    #
+    # - zsh-abbr
+    # - ls colored
+    # - prompt
+    # - plugins from old zshrc
+    # - things from old zshenv perhaps
+    # - environment link /share/zsh?
+    # 
+    # home.file = {
+    #   ".zshrc".source = ./zshrc;
+    #   ".zshenv".source = ./zshenv;
+    # };
+    programs.zsh = {
+      enable = true;
+      enableAutosuggestions = true;
+      enableCompletion = true;
+      enableSyntaxHighlighting = true;
+      history = {
+        size = 50000;
+        share = true;
+        expireDuplicatesFirst = true;
+        ignoreDups = true;
+        ignorePatterns = [ "rm *" "kill *" "pkill *" ];
+      };
     };
-    programs.zsh.enable = true;
   };
 }
