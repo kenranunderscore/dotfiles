697b81db089386c29dfa7733a3c76c0a69c78dfd
Johannes Maier <johannes.maier@mailbox.org>
2022-03-20 22:08:36 +0100

    zsh: improve some aspects

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -5,14 +5,13 @@ in {
   options.modules.shell.zsh.enable = lib.mkEnableOption "zsh";
 
   config = lib.mkIf cfg.enable {
+    # Packages that I want aliases to use.
+    home.packages = with pkgs; [ exa ];
+
     # TODO(Johannes):
-    #
-    # - select-word-style and other common options
-    # - tabbing with completion -> highlight current match, fzf-tab
-    # - in general: autoloading functions
     # - functions, how to autoload?
-    # - ls colored, exa instead?
     # - prompt
+    # - flakes for pinning plugins
     programs.zsh = let
       zsh-autopair = pkgs.fetchFromGitHub {
         owner = "hlissner";
@@ -42,13 +41,13 @@ in {
       enable = true;
       dotDir = ".config/zsh";
       enableCompletion = true;
-      # I chose to manager most of the plugins myself, by pinning the
-      # sources (TODO: use flakes for this) and sourcing the files.
-      # This gives better control over the versions and makes the
-      # ordering explicit.
+      # I choose to manage most of the plugins myself, by pinning the
+      # sources and sourcing the files.  This gives better control
+      # over the versions and makes the ordering explicit.  These
+      # enable* options are sugar for enabling certain plugins, so
+      # don't set them.
       enableSyntaxHighlighting = false;
       enableAutosuggestions = false;
-      autocd = true;
       history = {
         size = 10000;
         save = 20000;
@@ -57,12 +56,36 @@ in {
         ignoreDups = true;
         ignorePatterns = [ "rm *" "kill *" "pkill *" ];
       };
+      # I know autocd is an option in programs.zsh but -- as with
+      # plugins and variables -- I don't like mixing methods.
+      initExtraFirst = ''
+        setopt auto_cd
+      '';
       initExtra = ''
         source ${zsh-autopair}/autopair.zsh
-        source ${zsh-abbr}/zsh-abbr.zsh
-        source ${zsh-syntax-highlighting}/zsh-syntax-highlighting.zsh
+        autopair-init
         source ${zsh-autosuggestions}/zsh-autosuggestions.zsh
+        source ${zsh-syntax-highlighting}/zsh-syntax-highlighting.zsh
+        # Need to source zsh-abbr after z-sy-h, otherwise there's subtle
+        # breakage with the way autosuggestions are highlighted (?).
+        source ${zsh-abbr}/zsh-abbr.zsh
+
+        # Highlight current selection when completing
+        zstyle ':completion:*' menu select
       '';
+      # The one thing that's not as nice as in bash (but I don't have
+      # it in fish either): I cannot really distinguish between
+      # M-<backspace> and C-w, while in bash the former deletes
+      # "file-wise" in a path, and the latter doesn't.  This makes it
+      # so deletions stop at a / character.
+      localVariables = { WORDCHARS = "\${WORDCHARS/\\/}"; };
+      shellGlobalAliases = {
+        ls = "exa";
+        l = "exa -lbF --git --group-directories-first --icons";
+        ll = "exa -lbGF --git --group-directories-first --icons";
+        la = "exa -labF --git --group-directories-first --icons";
+        lla = "exa -labGF --git --group-directories-first --icons";
+      };
     };
 
     # Create shell abbreviations (akin to what fish does) from the set
