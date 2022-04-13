9fdfe1d21a05e7c4952dac390d70e09a4fa608ff
Johannes Maier <johannes.maier@active-group.de>
2022-04-13 17:18:32 +0200

    nyxt: only wrap a binary that I locally built from source
    This way, I get the latest version that "just works" in every regard.
    The only downside is that I have to wrap it in a nix-shell invocation.

--- modules/programs/nyxt.nix
+++ modules/programs/nyxt.nix
@@ -3,44 +3,13 @@
 let
   cfg = config.modules.programs.nyxt;
 
-  name = "nyxt-bin";
-  desktopItem = pkgs.makeDesktopItem {
-    inherit name;
-    desktopName = "Nyxt";
-    genericName = name;
-    exec = "nyxt";
-    comment = "The hacker's power browser";
-    icon = "nyxt";
-  };
-
-  # The currently packaged version of Nyxt is broken due to some
-  # FHS-related issue.  So for now I download the current release
-  # version and put in in the nix store manually.
-  nyxtBin = pkgs.stdenv.mkDerivation {
-    inherit name;
-    src = builtins.fetchurl {
-      url =
-        "https://github.com/atlas-engineer/nyxt/releases/download/2.2.4/nyxt-2.2.4.tar.xz";
-      sha256 = "1m47hgqdk1rs4dl9pndiiiqx4v3n5r5m8m3n8xqslzp388hab1fg";
-    };
-    unpackPhase = "tar xf $src";
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -r * $out
-      ln -sf $out/usr/local/bin/nyxt $out/bin/nyxt
-      mkdir -p $out/share/icons
-      cp -r ${desktopItem}/share/applications $out/share/
-      find gnu -type d -regex '.*nyxt.*/share/icons/hicolor' | xargs cp -r -t $out/share/icons/
-    '';
-    dontFixup = true;
-  };
+  # To get the most recent version I build Nyxt from source locally.
+  # This wrapper script assumes that the repo is cloned at
+  # ~/projects/forks/nyxt.
+  nyxtWrapped = pkgs.writeShellScriptBin "nyxt"
+    "nix-shell ~/projects/forks/nyxt/build-scripts/shell.nix --run '~/projects/forks/nyxt/nyxt'";
 in {
   options.modules.programs.nyxt.enable = lib.mkEnableOption "nyxt";
 
-  config = lib.mkIf cfg.enable {
-    home = {
-      packages = [ nyxtBin ];
-      sessionVariables = { WEBKIT_FORCE_SANDBOX = 0; };
-    };
-  };
+  config = lib.mkIf cfg.enable { home.packages = [ nyxtWrapped ]; };
 }
