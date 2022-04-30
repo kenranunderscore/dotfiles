e1a4086b835cb267cc517a8ed8559b0bf1aa1572
Johannes Maier <johannes.maier@mailbox.org>
2022-04-30 18:54:03 +0200

    ssh: add DCSS servers for remote console play

--- modules/shell/ssh.nix
+++ modules/shell/ssh.nix
@@ -27,6 +27,35 @@ in {
           hostname = "diablo";
           user = "ag";
         };
+
+        "cxc" = {
+          host = "cxc";
+          hostname = "crawl.xtahua.com";
+          user = "crawl";
+          compression = true;
+        };
+
+        "cdo" = {
+          host = "cdo";
+          hostname = "crawl.develz.org";
+          user = "crawl";
+          compression = true;
+        };
+
+        "cao" = {
+          host = "cao";
+          hostname = "crawl.akrasiac.org";
+          user = "joshua";
+          compression = true;
+        };
+
+        "cue" = {
+          host = "cue";
+          hostname = "underhound.eu";
+          port = 23;
+          user = "terminal";
+          compression = true;
+        };
       };
     };
   };
