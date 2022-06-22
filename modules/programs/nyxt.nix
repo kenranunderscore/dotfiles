7339ce34781dd3b47ecc68a02760a997f7a3025a
Johannes Maier <johannes.maier@active-group.de>
2022-06-22 15:14:26 +0200

    nyxt: repair

--- modules/programs/nyxt.nix
+++ modules/programs/nyxt.nix
@@ -6,8 +6,10 @@ let
   # To get the most recent version I build Nyxt from source locally.
   # This wrapper script assumes that the repo is cloned at
   # ~/projects/forks/nyxt.
-  nyxtWrapped = pkgs.writeShellScriptBin "nyxt"
-    "nix-shell ~/projects/forks/nyxt/build-scripts/shell.nix --run '~/projects/forks/nyxt/nyxt'";
+  nyxtWrapped = pkgs.writeShellScriptBin "nyxt" ''
+    export WEBKIT_DISABLE_COMPOSITING_MODE=1
+    nix-shell ~/projects/forks/nyxt/build-scripts/shell.nix --run '~/projects/forks/nyxt/nyxt'
+  '';
 in {
   options.modules.programs.nyxt.enable = lib.mkEnableOption "nyxt";
 
