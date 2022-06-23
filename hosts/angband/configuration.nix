0bb360567a932cd417a53f15dd9a5f9e753f73f4
Johannes Maier <johannes.maier@mailbox.org>
2022-06-23 19:49:40 +0200

    angband: add
    Hardware configuration needs to be added on the actual machine.

---
+++ hosts/angband/configuration.nix
@@ -0,0 +1,99 @@
+{ config, pkgs, customConfig, ... }:
+
+let username = customConfig.username;
+in {
+  nix = {
+    package = pkgs.nixUnstable;
+    extraOptions = ''
+      experimental-features = nix-command flakes
+    '';
+    settings.trusted-users = [ "root" username ];
+  };
+  nixpkgs.config.allowUnfree = true;
+
+  imports = [ ./hardware-configuration.nix ];
+
+  boot = {
+    kernelPackages = pkgs.linuxPackages_latest;
+    loader.systemd-boot.enable = true;
+    loader.efi.canTouchEfiVariables = true;
+  };
+
+  networking = {
+    hostName = "angband";
+    networkmanager.enable = true;
+  };
+
+  time.timeZone = "Europe/Berlin";
+
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "us";
+  };
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  services = {
+    xserver = {
+      enable = true;
+      libinput = {
+        enable = true;
+        mouse.naturalScrolling = true;
+      };
+      layout = "us";
+      xkbVariant = "altgr-intl";
+      displayManager = {
+        session = [{
+          manage = "window";
+          name = "fake";
+          start = "";
+        }];
+        defaultSession = "none+fake";
+        autoLogin = {
+          enable = true;
+          user = username;
+        };
+        lightdm = {
+          enable = true;
+          greeters.mini.enable = true;
+          greeters.mini.user = username;
+        };
+      };
+    };
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+      alsa.enable = false;
+    };
+  };
+
+  programs.ssh.startAgent = true;
+
+  documentation = {
+    enable = true;
+    man.enable = true;
+    dev.enable = true;
+  };
+
+  hardware = { opengl.enable = true; };
+
+  users.users.${username} = {
+    isNormalUser = true;
+    home = "/home/${username}";
+    extraGroups = [ "wheel" "networkmanager" ];
+  };
+
+  environment = {
+    systemPackages = with pkgs; [ ];
+    pathsToLink = [ "/share/zsh" ];
+  };
+
+  networking.firewall.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+}
---
+++ hosts/angband/home.nix
@@ -0,0 +1,80 @@
+{ inputs, config, lib, pkgs, ... }:
+
+{
+  imports = [ ../base.nix ../../modules ];
+
+  hosts.base = {
+    privateDir = "${inputs.privateConfig}";
+    shellPath = "${pkgs.zsh}/bin/zsh";
+    gpgKey = "0BAD1500D7D4282C433BC0BC9AC78C1A48681583";
+  };
+
+  modules = {
+    desktop = {
+      rofi.enable = true;
+      i3 = {
+        enable = true;
+        workspaces = [
+          { name = "1:main/emacs"; }
+          {
+            name = "2:web";
+            assigns = [{ class = "firefox"; }];
+          }
+          { name = "3"; }
+          { name = "4"; }
+          { name = "5:chat"; }
+          { name = "6"; }
+          { name = "7"; }
+          { name = "8"; }
+          { name = "9"; }
+        ];
+        startupCommands = [
+          {
+            command = "~/.fehbg";
+            always = false;
+            notification = false;
+          }
+          {
+            # Workaround: otherwise i3 module will be empty at first
+            command = "systemctl --user restart polybar";
+            notification = false;
+          }
+          {
+            command = "xset r rate 200 55";
+            always = true;
+            notification = false;
+          }
+        ];
+      };
+      polybar.enable = true;
+      picom.enable = true;
+    };
+    email = {
+      certificatesFile = "/etc/ssl/certs/ca-certificates.crt";
+      primaryAccount = "mailbox";
+    };
+    fonts.enable = true;
+    programs = {
+      emacs.emacsVersion = "stable";
+      kitty = { useLoginShell = false; };
+      nyxt.enable = true;
+      sbcl.enable = true;
+    };
+    shell.git.email = "johannes.maier@mailbox.org";
+  };
+
+  services = {
+    syncthing.enable = true;
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+    };
+  };
+
+  xsession.enable = true;
+
+  home = {
+    packages = with pkgs; [ cmus cmusfm element-desktop ];
+    stateVersion = "21.03";
+  };
+}
