142f1371c853bdaad7c94123e65e9b5dc6a98cf0
Johannes Maier <johannes.maier@mailbox.org>
2022-11-16 21:08:59 +0100

    neovim: configure neogit status buffer

--- home-manager-modules/programs/neovim/nvim/after/plugin/neogit.lua
+++ home-manager-modules/programs/neovim/nvim/after/plugin/neogit.lua
@@ -4,6 +4,16 @@ neogit.setup {
     disable_hints = false,
     disable_context_highlighting = false,
     disable_commit_confirmation = true,
+    disable_builtin_notifications = false,
+    sections = {
+        stashes = false,
+        recent = false,
+    },
+    signs = {
+        section = { "", "" },
+        item = { "", "" },
+        hunk = { "", "" },
+    },
 }
 
 local nnoremap = require("kenran.remap").nnoremap
