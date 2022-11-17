3ed85adabc4c58e3d198611cd56864cd02d1cf1e
Johannes Maier <johannes.maier@active-group.de>
2022-11-17 12:56:49 +0100

    neovim/cmp: configure keybindings

--- home-manager-modules/programs/neovim/nvim/after/plugin/cmp.lua
+++
@@ -1,7 +0,0 @@
-local cmp = require("cmp")
-
-cmp.setup {
-    sources = cmp.config.sources(
-    { { name = "nvim_lsp" }, { name = "vsnip" } },
-    { name = "buffer" })
-}
---
+++ home-manager-modules/programs/neovim/nvim/after/plugin/completion.lua
@@ -0,0 +1,23 @@
+local cmp = require("cmp")
+local luasnip = require("luasnip")
+
+cmp.setup {
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    sources = cmp.config.sources({
+        { name = "luasnip" },
+        { name = "buffer" },
+        { name = "path" },
+    }),
+    mapping = cmp.mapping.preset.insert({
+        ["<down>"] = cmp.mapping.select_next_item(),
+        ["<tab>"] = cmp.mapping.select_next_item(),
+        ["<up>"] = cmp.mapping.select_prev_item(),
+        ["<cr>"] = cmp.mapping.confirm({ select = false }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    }),
+}
