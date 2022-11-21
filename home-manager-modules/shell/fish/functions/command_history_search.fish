4e446eb6c26d888e4912fe8dde9fb8d06bd76185
Johannes Maier <johannes.maier@mailbox.org>
2022-11-21 22:24:19 +0100

    fish: fix a `test` call

--- home-manager-modules/shell/fish/functions/command_history_search.fish
+++ home-manager-modules/shell/fish/functions/command_history_search.fish
@@ -2,7 +2,7 @@ function command_history_search \
     --description 'Reverse-search the command history'
     set fzf fzf --no-sort --exact --prompt "Command: "
     set command (history | $fzf)
-    if test -n $command
+    if test -n "$command"
         commandline $command
     end
     commandline -f repaint
