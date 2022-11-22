a75c4e57f1ce3806d377d4d81ab2a10582265d5a
Johannes Maier <johannes.maier@active-group.de>
2022-11-22 17:37:35 +0100

    neovim: move settings into init.lua
    This fixes any load order problems, as the Lua files inside plugin/ are
    loaded in alphabetical order.

--- home-manager-modules/programs/neovim/nvim/init.lua
+++ home-manager-modules/programs/neovim/nvim/init.lua
@@ -1,1 +1,33 @@
--- everything is loaded automatically from ./plugin/
+-- These settings should be loaded before everything else.  Hopefully that's
+-- the case.
+
+-- SPC as leader key
+vim.g.mapleader = " "
+
+local opt = vim.opt
+
+opt.nu = true
+opt.relativenumber = true
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+opt.hlsearch = true
+opt.incsearch = true
+opt.ignorecase = true
+opt.smartcase = true
+
+opt.wrap = false
+
+opt.guicursor = ""
+opt.termguicolors = true
+
+opt.signcolumn = "yes"
+
+opt.completeopt = "menu,menuone,noselect"
+
+vim.g.autoformat = 1
+
+opt.exrc = true
