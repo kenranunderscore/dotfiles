4f62513858006e5177825ae30cfeebdc7f17a4a6
Johannes Maier <johannes.maier@active-group.de>
2022-11-22 09:05:03 +0100

    neovim: enable project-local configuration

--- home-manager-modules/programs/neovim/nvim/lua/kenran/init.lua
+++ home-manager-modules/programs/neovim/nvim/lua/kenran/init.lua
@@ -2,3 +2,11 @@ require("kenran.set")
 require("kenran.plugins")
 require("kenran.general_keymap")
 require("kenran.format")
+
+-- work around .exrc not loading when using init.lua instead of init.vim
+-- TODO: switch to vim.secure.read(), once I know how to source the result
+if vim.loop.fs_stat(".nvimrc") then
+    vim.cmd('sandbox source .nvimrc')
+elseif vim.loop.fs_stat(".exrc") then
+    vim.cmd('sandbox source .exrc')
+end
