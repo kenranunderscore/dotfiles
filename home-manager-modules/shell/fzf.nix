2bb98989282407cb3600b93f8dbd6690cb156c30
Johannes Maier <johannes.maier@active-group.de>
2022-11-25 13:08:23 +0100

    fzf: use identical border style for windows

--- home-manager-modules/shell/fzf.nix
+++ home-manager-modules/shell/fzf.nix
@@ -8,7 +8,8 @@ in {
     home = {
       packages = [ pkgs.fzf ];
       sessionVariables = {
-        FZF_DEFAULT_OPTS = "--border sharp --height 40% --layout reverse --select-1 --exit-0";
+        FZF_DEFAULT_OPTS =
+          "--border sharp --height 40% --layout reverse --select-1 --exit-0 --preview-window ',border-sharp'";
         FZF_ALT_C_OPTS = "--preview 'tree -C {} | head -n 50'";
         FZF_CTRL_T_OPTS = "--prompt 'Choose file: '";
         FZF_TMUX = "1";
