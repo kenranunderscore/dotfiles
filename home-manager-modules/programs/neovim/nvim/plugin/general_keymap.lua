a05f97a53bc3c1e351702c550e6d86f16330c582
Johannes Maier <johannes.maier@active-group.de>
2022-11-25 15:56:24 +0100

    neovim: vertically center after `G`

--- home-manager-modules/programs/neovim/nvim/plugin/general_keymap.lua
+++ home-manager-modules/programs/neovim/nvim/plugin/general_keymap.lua
@@ -1,15 +1,19 @@
 local nnoremap = require("kenran.remap").nnoremap
 
-nnoremap("<leader>pd", "<cmd>Ex<cr>")
+-- vertically center the current line after certain jumps (thanks prime)
 nnoremap("<C-u>", "<C-u>zz")
 nnoremap("<C-d>", "<C-d>zz")
 nnoremap("n", "nzz")
 nnoremap("N", "Nzz")
 nnoremap("*", "*zz")
 nnoremap("#", "#zz")
+nnoremap("G", "Gzz")
 
 -- quickly delete the current buffer
 nnoremap("<leader>bq", "<cmd>bd<cr>")
+
+-- open netrw in the current pwd
+nnoremap("<leader>pd", "<cmd>Ex<cr>")
 
 -- remove search highlighting on ESC
 nnoremap("<esc>", "<cmd>noh<cr>")
