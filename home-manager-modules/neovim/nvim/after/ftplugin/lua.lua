00d8f1ed28908b7cf122e7ef686b0e594921e00f
Johannes Maier <johannes.maier@mailbox.org>
2022-12-18 23:02:15 +0100

    neovim/autoformat: use undojoin to try and prevent buffer replacement

--- home-manager-modules/neovim/nvim/after/ftplugin/lua.lua
+++ home-manager-modules/neovim/nvim/after/ftplugin/lua.lua
@@ -1,1 +1,2 @@
-vim.opt_local.formatprg = "stylua --config-path ./.stylua.toml --stdin-filepath % -"
+vim.opt_local.formatprg =
+    "tee /tmp/format_lua | stylua --config-path ./.stylua.toml --stdin-filepath % 2>/dev/null - || cat /tmp/format_lua"
--- home-manager-modules/neovim/nvim/plugin/format.lua
+++ home-manager-modules/neovim/nvim/plugin/format.lua
@@ -3,6 +3,7 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
         if vim.g.autoformat == 1 and vim.bo.formatprg ~= "" then
             local view = vim.fn.winsaveview()
+            vim.cmd("undojoin")
             vim.cmd("exe 'norm gggqG'")
             vim.fn.winrestview(view)
         end
