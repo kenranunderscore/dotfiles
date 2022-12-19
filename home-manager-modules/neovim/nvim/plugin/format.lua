5f684a7417240e43e35b085227f7c3c2e47a40ac
Johannes Maier <johannes.maier@active-group.de>
2022-12-19 14:31:51 +0100

    neovim/format: remove undojoin
    It sometimes leads to even more errors than without it, so zap for now.

--- home-manager-modules/neovim/nvim/plugin/format.lua
+++ home-manager-modules/neovim/nvim/plugin/format.lua
@@ -3,7 +3,6 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
         if vim.g.autoformat == 1 and vim.bo.formatprg ~= "" then
             local view = vim.fn.winsaveview()
-            vim.cmd("undojoin")
             vim.cmd("exe 'norm gggqG'")
             vim.fn.winrestview(view)
         end
