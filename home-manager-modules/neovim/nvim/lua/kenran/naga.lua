070e38fba8350e24eb8d3aad9b35372197be5866
Johannes Maier <johannes.maier@mailbox.org>
2022-12-22 16:36:43 +0100

    neovim/naga: update

--- home-manager-modules/neovim/nvim/lua/kenran/naga.lua
+++ home-manager-modules/neovim/nvim/lua/kenran/naga.lua
@@ -6,12 +6,24 @@ local colors = {
     fg = "#0ac30a",
     fg_dark = "#078807",
     yellow = "#eec900",
+    gold = "#eead0e",
     cyan = "#00cdcd",
     string = "#b3ee3a",
+    purple = "#cc59d2",
     orange = "#ff9000",
     comment = "#707370",
-    blue = "#01018a",
-    purple = "#cc59d2",
+    comment_light = "#099590",
+    comment_dark = "#353835",
+    docstring = "#698b22",
+    gray = "#aabaaa",
+    dark_blue = "#01018a",
+    sea_green = "#3cb371",
+    orange_red = "#ff4500",
+    red = "#ff1500",
+    whitespace_fg = "#555f55",
+    block = "#060606",
+    block_light = "#252525",
+    olivedrab3 = "#9acd32",
 }
 
 function M.colorscheme()
@@ -40,7 +52,7 @@ function M.colorscheme()
 
     vim.api.nvim_set_hl(0, "Fg", { fg = colors.fg })
     vim.api.nvim_set_hl(0, "Grey", { fg = colors.comment })
-    vim.api.nvim_set_hl(0, "Red", { fg = "#f00000" })
+    vim.api.nvim_set_hl(0, "Red", { fg = colors.red })
     vim.api.nvim_set_hl(0, "Orange", { fg = colors.orange })
     vim.api.nvim_set_hl(0, "Yellow", { fg = colors.yellow })
     vim.api.nvim_set_hl(0, "Green", { fg = colors.fg })
@@ -48,6 +60,39 @@ function M.colorscheme()
     vim.api.nvim_set_hl(0, "Purple", { fg = colors.purple })
     vim.api.nvim_set_hl(0, "Normal", { fg = colors.fg })
     vim.api.nvim_set_hl(0, "Grey", { fg = colors.comment })
+
+    vim.api.nvim_set_hl(0, "Constant", { fg = colors.purple })
+    vim.api.nvim_set_hl(0, "Type", { fg = colors.olivedrab3 })
+    vim.api.nvim_set_hl(0, "PreProc", { fg = colors.orange })
+    vim.api.nvim_set_hl(0, "String", { fg = colors.string })
+    vim.api.nvim_set_hl(0, "Character", { fg = colors.string })
+    vim.api.nvim_set_hl(0, "Special", { fg = colors.docstring })
+    vim.api.nvim_set_hl(0, "Identifier", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "Operator", { fg = colors.fg })
+    vim.api.nvim_set_hl(0, "Statement", { fg = colors.cyan })
+    vim.api.nvim_set_hl(0, "Keyword", { fg = colors.yellow })
+    vim.api.nvim_set_hl(0, "Comment", { fg = colors.comment })
+
+    vim.api.nvim_set_hl(0, "NonText", { fg = colors.whitespace_fg })
+    vim.api.nvim_set_hl(0, "Visual", { fg = colors.fg, bg = colors.dark_blue })
+    vim.api.nvim_set_hl(0, "Search", { fg = colors.bg, bg = colors.fg_dark })
+    vim.api.nvim_set_hl(0, "IncSearch", { fg = colors.bg, bg = colors.fg })
+    vim.api.nvim_set_hl(0, "LineNr", { fg = colors.block_light })
+    vim.api.nvim_set_hl(0, "LineNrAbove", { fg = colors.block_light })
+    vim.api.nvim_set_hl(0, "LineNrBelow", { fg = colors.block_light })
+    vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#006400" })
+    vim.api.nvim_set_hl(0, "MatchParen", { bg = colors.red })
+    vim.api.nvim_set_hl(0, "Todo", { fg = colors.purple, bg = colors.bg, bold = true, underline = true })
+
+    vim.api.nvim_set_hl(0, "Pmenu", { bg = "#080808" })
+    vim.api.nvim_set_hl(0, "PmenuSel", { bg = colors.dark_blue, fg = colors.fg })
+    vim.api.nvim_set_hl(0, "PmenuSbar", { bg = colors.comment_dark })
+    vim.api.nvim_set_hl(0, "PmenuThumb", { bg = colors.fg })
+    vim.api.nvim_set_hl(0, "TabLine", { fg = colors.comment_dark, bg = colors.bg, italic = true })
+    vim.api.nvim_set_hl(0, "TabLineSel", { fg = colors.fg_dark, bg = colors.bg, underline = true })
+    vim.api.nvim_set_hl(0, "TabLineFill", { bg = colors.bg })
+    vim.api.nvim_set_hl(0, "CursorLine", { bg = colors.bg_green })
+    vim.api.nvim_set_hl(0, "CursorColumn", { bg = colors.bg_green })
 end
 
 return M
