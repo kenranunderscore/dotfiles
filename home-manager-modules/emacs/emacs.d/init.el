c52dd6d5ca2a16f842c8614b97c521c33e70f01f
Johannes Maier <johannes.maier@active-group.de>
2023-03-21 23:04:51 +0100

    emacs: make init.el more robust

--- home-manager-modules/emacs/emacs.d/init.el
+++ home-manager-modules/emacs/emacs.d/init.el
@@ -2,17 +2,20 @@
 
 (defun kenran/tangle-org-file (path)
   "Tangle an org file at PATH relative to the `user-emacs-directory'
-to an ELisp file."
-  (let* ((absolute-path (locate-user-emacs-file path))
-         (target (replace-regexp-in-string "\.org$" ".el" absolute-path)))
+to an ELisp file.  Will only tangle if no target file exists yet,
+or if the source file has been touched more recently than the
+target."
+  (let* ((target (replace-regexp-in-string "\.org$" ".el" path)))
     (when (or (not (file-exists-p target))
-              (file-newer-than-file-p absolute-path target))
-      (org-babel-tangle-file absolute-path target))))
+              (file-newer-than-file-p path target))
+      (org-babel-tangle-file path target))))
 
-;; Load my org-mode configuration.  If its timestamp is newer than
-;; that of the result of tangling it into ELisp, then trigger a
-;; recreation of the ELisp config.
-(kenran/tangle-org-file "my-packages/my-haskell.org")
-(kenran/tangle-org-file "config.org")
+;; Tangle all my custom packages.
+(setq kenran/custom-package-dir
+      (concat user-emacs-directory "my-packages"))
+(dolist (f (directory-files kenran/custom-package-dir
+                            :match "\.org$"))
+  (kenran/tangle-org-file f))
 
-(load-file (locate-user-emacs-file "config.el"))
+(kenran/tangle-org-file (concat user-emacs-directory "config.org"))
+(load-file (concat user-emacs-directory "config.el"))
