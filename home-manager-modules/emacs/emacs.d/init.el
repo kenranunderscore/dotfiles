57c9e5c912704755aaaeb67b334e8487bb3e617a
Johannes Maier <johannes.maier@mailbox.org>
2023-03-30 08:19:06 +0200

    emacs: simplify/rename custom "package" prefix
    kenran/ -> my/
    
    It's easier to search for, and shorter.

--- home-manager-modules/emacs/emacs.d/init.el
+++ home-manager-modules/emacs/emacs.d/init.el
@@ -1,6 +1,6 @@
 (require 'org)
 
-(defun kenran/tangle-org-file (path)
+(defun my/tangle-org-file (path)
   "Tangle an org file at PATH relative to the `user-emacs-directory'
 to an ELisp file.  Will only tangle if no target file exists yet,
 or if the source file has been touched more recently than the
@@ -14,11 +14,11 @@ target."
       (set-file-times target))))
 
 ;; Tangle all my custom packages.
-(setq kenran/custom-package-dir
+(setq my/custom-package-dir
       (concat user-emacs-directory "my-packages"))
-(dolist (f (directory-files kenran/custom-package-dir
+(dolist (f (directory-files my/custom-package-dir
                             :match "\.org$"))
-  (kenran/tangle-org-file f))
+  (my/tangle-org-file f))
 
-(kenran/tangle-org-file (concat user-emacs-directory "config.org"))
+(my/tangle-org-file (concat user-emacs-directory "config.org"))
 (load-file (concat user-emacs-directory "config.el"))
