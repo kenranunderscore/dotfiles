dcef14b49a47c25e78b0271737be3b368dcdcbc6
Johannes Maier <johannes.maier@active-group.de>
2023-09-10 21:07:57 +0200

    doom: configure org-mode (and other tweaks)

--- home-manager-modules/doom/doom/packages.el
+++ home-manager-modules/doom/doom/packages.el
@@ -6,16 +6,14 @@
 (package! emacs-naga-theme
   :recipe (:host github :repo "kenranunderscore/emacs-naga-theme"
            :files ("naga-theme.el")))
-
 (package! modus-themes)
-
 (package! gruber-darker-theme)
-
 (package! srcery-theme)
 
 ;;; Org mode
 
 (package! org-appear)
+(package! org-modern)
 
 ;;; E-mail
 
