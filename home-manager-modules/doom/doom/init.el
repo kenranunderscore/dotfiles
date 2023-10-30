329f6d1f2648e95d07cfb824c9d09662038ddd70
Johannes Maier <johannes.maier@mailbox.org>
2023-10-30 21:39:05 +0100

    doom/crystal: add

--- home-manager-modules/doom/doom/init.el
+++ home-manager-modules/doom/doom/init.el
@@ -120,7 +120,7 @@
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
-       ;;crystal           ; ruby at the speed of c
+       crystal           ; ruby at the speed of c
        ;;csharp            ; unity, .NET, and mono shenanigans
        ;;data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
