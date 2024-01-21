628c4a9d57af7e82898b8eb5595a37a437481a16
Johannes Maier <johannes.maier@mailbox.org>
2024-01-21 08:54:13 +0100

    xdg: make ghcup comply

--- home-manager-modules/xdg/default.nix
+++ home-manager-modules/xdg/default.nix
@@ -8,5 +8,8 @@
   # variables are set. Let's put all of those here; they're not really annoying
   # even if I don't use the corresponding programs much or at all anymore, but
   # prevent cluttering of $HOME in case I quickly try something.
-  home.sessionVariables = { STACK_XDG = "1"; };
+  home.sessionVariables = {
+    STACK_XDG = "1";
+    GHCUP_USE_XDG_DIRS = "1";
+  };
 }
