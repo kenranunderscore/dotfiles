64b1eaae3a50ae7389c02364686b867e75863d49
Johannes Maier <johannes.maier@mailbox.org>
2024-03-08 13:33:06 +0100

    fonts: add Berkeley Mono
    Finally bought it! With v2 being on the horizon as a free upgrade
    there's no reason to wait :)

--- home-manager-modules/wezterm/wezterm.lua
+++ home-manager-modules/wezterm/wezterm.lua
@@ -4,7 +4,7 @@ local config = {}
 
 config.enable_tab_bar = false
 config.color_scheme = 'Everforest Dark (Gogh)'
-config.font = wezterm.font 'JetBrains Mono'
+config.font = wezterm.font 'Berkeley Mono'
 config.font_size = 16.0
 config.audible_bell = "Disabled"
 config.default_cursor_style = "SteadyBlock"
--- hosts/tehanu/custom.nix
+++ hosts/tehanu/custom.nix
@@ -1,7 +1,7 @@
 {
   username = "johannes";
   font = {
-    name = "JetBrains Mono";
-    size = 15;
+    name = "Berkeley Mono";
+    size = 16;
   };
 }
