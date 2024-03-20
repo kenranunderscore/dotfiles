2927b7dc26e8eed6280655392dff4fb68b6ec83c
Johannes Maier <johannes.maier@mailbox.org>
2024-03-20 13:32:39 +0100

    wezterm: add option to only install the config file
    I might do something similar for all the things that are able to use a
    GPU but have problems finding the OpenGL libraries on non-NixOS, like
    kitty.
    
    That way I can work without having to use nixGL, but have to install
    these tools with the system package manager (which shouldn't be a
    problem at all).

--- home-manager-modules/wezterm/default.nix
+++ home-manager-modules/wezterm/default.nix
@@ -4,10 +4,16 @@ let
   cfg = config.modules.wezterm;
   types = lib.types;
 in {
-  options.modules.wezterm.enable = lib.mkEnableOption "wezterm";
+  options.modules.wezterm = {
+    enable = lib.mkEnableOption "wezterm";
+    withPackage = lib.mkOption {
+      default = true;
+      type = lib.types.bool;
+    };
+  };
 
   config = lib.mkIf cfg.enable {
-    home.packages = [ pkgs.wezterm ];
+    home.packages = lib.optional cfg.withPackage pkgs.wezterm;
     xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
   };
 }
