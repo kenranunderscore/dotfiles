fbd42cd4ba81326308d5798f682ff57e470c4732
Johannes Maier <johannes.maier@mailbox.org>
2025-04-15 16:00:21 +0200

    fzf: behave more like emacs/vertico

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -15,7 +15,7 @@ in
     home = {
       packages = [ pkgs.fzf ];
       sessionVariables = {
-        FZF_DEFAULT_OPTS = "--border sharp --height 40% --layout reverse --select-1 --exit-0 --preview-window ',border-sharp'";
+        FZF_DEFAULT_OPTS = "--exact --extended --border sharp --height 40% --layout reverse --select-1 --exit-0 --preview-window ',border-sharp'";
         FZF_ALT_C_OPTS = "--preview 'tree -C {} | head -n 50'";
         FZF_CTRL_T_OPTS = "--prompt 'Choose file: '";
         FZF_TMUX = "1";
