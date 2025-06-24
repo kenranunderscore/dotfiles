aa5b9be1a827b378b39b17c002426d05bb747ac5
Johannes Maier <johannes.maier@mailbox.org>
2025-06-24 17:20:30 +0200

    fish/fisher: fix fish_complete_path

--- modules/fish/conf.d/fisher.fish
+++ modules/fish/conf.d/fisher.fish
@@ -1,7 +1,7 @@
 set -U fisher_path $__fish_config_dir/fisher
 
-set fish_function_path $fish_function_path[1] $fisher_path/completions $fish_function_path[2..]
 set fish_function_path $fish_function_path[1] $fisher_path/functions $fish_function_path[2..]
+set fish_complete_path $fish_complete_path[1] $fisher_path/completions $fish_complete_path[2..]
 for file in $fisher_path/conf.d/*.fish
     source $file
 end
