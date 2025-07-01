72bcaf564bc16c008fba072455a4db948ac0b792
Johannes Maier <johannes.maier@mailbox.org>
2025-07-01 22:27:46 +0200

    neovim: update options

--- modules/neovim/nvim/lua/config/options.lua
+++ modules/neovim/nvim/lua/config/options.lua
@@ -1,2 +1,20 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = ","
+
+local opt = vim.opt
+
+-- use relative line numbers
+opt.nu = true
+opt.relativenumber = true
+
+-- indentation
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+
+-- always show the signcolumn to prevent horizontal buffer movement
+opt.signcolumn = "yes"
+
+opt.list = true
+opt.listchars = "tab:» ,nbsp:+"
