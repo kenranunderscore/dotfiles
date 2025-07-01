02d128b1782c40336299b1fd412e78fc8bbcde0c
Johannes Maier <johannes.maier@mailbox.org>
2025-07-01 22:24:11 +0200

    neovim/heresy: save with <c-s>

--- modules/neovim/nvim/lua/config/keymap.lua
+++ modules/neovim/nvim/lua/config/keymap.lua
@@ -1,6 +1,6 @@
 local map = vim.keymap.set
 
 map("n", "<esc>", "<cmd>noh<cr>")
-
+map({ "n", "i" }, "<C-s>", "<esc><cmd>w<cr>")
 map({ "n", "i" }, "<C-l>", "<C-w><C-w>")
 map("n", "<C-w><C-d>", "<C-w>q")
