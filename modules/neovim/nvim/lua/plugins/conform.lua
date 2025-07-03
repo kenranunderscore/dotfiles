7812832fbc8dc7345af51a7c7adc1d1e45f31b7c
Johannes Maier <johannes.maier@mailbox.org>
2025-07-03 06:35:37 +0200

    neovim/conform: add

---
+++ modules/neovim/nvim/lua/plugins/conform.lua
@@ -0,0 +1,20 @@
+return {
+  "stevearc/conform.nvim",
+  opts = {
+    formatters_by_ft = {
+      elixir = { "mix" },
+      go = { "gofmt" },
+      haskell = { "fourmolu" },
+      lua = { "stylua" },
+      nix = { "nixfmt" },
+      ocaml = { "ocamlformat" },
+      python = { "isort", "black" },
+      rust = { "rustfmt", lsp_format = "fallback" },
+      zig = { "zigfmt" },
+    },
+    format_on_save = {
+      timeout_ms = 500,
+      lsp_format = "fallback",
+    },
+  },
+}
