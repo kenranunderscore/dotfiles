0e225db6fd6b05a343cc2b2c5519d5bd90cfb276
Johannes Maier <johannes.maier@mailbox.org>
2025-07-04 08:34:19 +0200

    alacritty: add themes

--- modules/alacritty/default.nix
+++ modules/alacritty/default.nix
@@ -16,6 +16,20 @@
         xdg = true;
       }
     ];
-    home.packages = [ pkgs.alacritty ];
+    home = {
+      packages = [ pkgs.alacritty ];
+      activation.downloadAlacrittyThemes = lib.hm.dag.entryAfter [ "symlinkCustomConfigFiles" ] ''
+        target_dir="$XDG_CONFIG_HOME/alacritty/themes"
+        if [ ! -d "$target_dir" ]; then
+          $DRY_RUN_CMD ${lib.getExe pkgs.gitMinimal} clone \
+                       --single-branch \
+                       https://github.com/alacritty/alacritty-theme \
+                       "$target_dir"
+        else
+          echo "Nothing to do"
+        fi
+      '';
+
+    };
   };
 }
