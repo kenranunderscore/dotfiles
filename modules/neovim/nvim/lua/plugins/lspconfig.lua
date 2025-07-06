9a8c4c8221867dc1fe8650722ee2c126d47edc4f
Johannes Maier <johannes.maier@mailbox.org>
2025-07-06 11:06:15 +0200

    neovim/completion: coq -> cmp
    coq has issues in ghostty (or ghostty does with coq). nvim-cmp's speed
    is fine for me, and it looks nicer, too.

--- modules/neovim/nvim/lua/plugins/lspconfig.lua
+++ modules/neovim/nvim/lua/plugins/lspconfig.lua
@@ -2,8 +2,7 @@ return {
   "neovim/nvim-lspconfig",
   lazy = false,
   dependencies = {
-    { "ms-jpq/coq_nvim", branch = "coq" },
-    { "ms-jpq/coq.artifacts", branch = "artifacts" },
+    { "hrsh7th/nvim-cmp" },
     { "maan2003/lsp_lines.nvim" },
     {
       "folke/lazydev.nvim",
@@ -17,11 +16,6 @@ return {
       },
     },
   },
-  init = function()
-    vim.g.coq_settings = {
-      auto_start = false,
-    }
-  end,
   config = function()
     require("lsp_lines").setup()
     vim.diagnostic.config({
@@ -42,34 +36,23 @@ return {
       vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled({ bufnr = 0 }), { bufnr = 0 })
     end)
 
-    local coq = require("coq")
     local lsp = vim.lsp
+    local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
     -- Lua
-    lsp.config("lua_ls", coq.lsp_ensure_capabilities({}))
+    lsp.config("lua_ls", { capabilities = capabilities })
     lsp.enable("lua_ls")
 
     -- Nix
-    lsp.config(
-      "nil_ls",
-      coq.lsp_ensure_capabilities({
-        autostart = true,
-        cmd = { "nil" },
-      })
-    )
+    lsp.config("nil_ls", { capabilities = capabilities })
     lsp.enable("nil_ls")
 
     -- Rust
-    lsp.config("rust_analyzer", coq.lsp_ensure_capabilities({}))
+    lsp.config("rust_analyzer", { capabilities = capabilities })
     lsp.enable("rust_analyzer")
 
     -- HTML/Emmet
-    lsp.config(
-      "emmet-language-server",
-      coq.lsp_ensure_capabilities({
-        cmd = { "emmet-language-server", "--stdio" },
-      })
-    )
+    lsp.config("emmet-language-server", { capabilities = capabilities })
     lsp.enable("emmet-language-server")
   end,
 }
