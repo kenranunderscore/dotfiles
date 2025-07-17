988e5eea1789feef0d22272869039e6ddb932131
Johannes Maier <johannes.maier@mailbox.org>
2025-07-17 07:10:49 +0200

    neovim/elixir: add defmodule snippet

---
+++ modules/neovim/nvim/lua/snippets/elixir.lua
@@ -0,0 +1,16 @@
+require("luasnip.session.snippet_collection").clear_snippets("go")
+
+local luasnip = require("luasnip")
+local s = luasnip.s
+local t = luasnip.t
+local i = luasnip.i
+
+luasnip.add_snippets("go", {
+  s("m", {
+    t("defmodule "),
+    i(1, "TheModule"),
+    t({ " do", "\t" }),
+    i(0),
+    t({ "", "end" }),
+  }),
+})
