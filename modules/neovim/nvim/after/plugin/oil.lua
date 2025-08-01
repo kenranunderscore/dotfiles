238a40da46fce7ccdce4ef87b119bc109e146b0e
Johannes Maier <johannes.maier@mailbox.org>
2025-08-01 07:44:33 +0200

    neovim/oil: show hidden files, but skip '..'

--- modules/neovim/nvim/after/plugin/oil.lua
+++ modules/neovim/nvim/after/plugin/oil.lua
@@ -1,4 +1,10 @@
 require("oil").setup {
+  view_options = {
+    show_hidden = true,
+    is_always_hidden = function(name, _)
+      return name == ".."
+    end,
+  },
   keymaps = {
     ["<C-s>"] = false,
   },
