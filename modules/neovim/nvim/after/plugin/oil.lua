3e69cc672e8010598d70fd84598e6774037a47ab
Johannes Maier <johannes.maier@mailbox.org>
2025-08-01 08:58:58 +0200

    neovim/oil: configure floating window

--- modules/neovim/nvim/after/plugin/oil.lua
+++ modules/neovim/nvim/after/plugin/oil.lua
@@ -5,9 +5,20 @@ require("oil").setup {
       return name == ".."
     end,
   },
+  float = {
+    padding = 10,
+    max_width = 0.5,
+    max_height = 0.5,
+    border = "rounded",
+  },
   keymaps = {
     ["<C-s>"] = false,
+    ["q"] = function()
+      require("oil").close()
+    end,
   },
 }
 
-vim.keymap.set({ "n", "v" }, "<leader>dd", "<cmd>Oil<cr>")
+vim.keymap.set({ "n", "v" }, "-", function()
+  require("oil").open_float()
+end)
