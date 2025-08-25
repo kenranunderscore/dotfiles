73574c6ae75fd94661954350e1a68a1acad0f7cc
Johannes Maier <johannes.maier@mailbox.org>
2025-08-25 08:12:07 +0200

    fish/prompt: drop unicode characters
    This ensures nice rendering and no visual artifacts when switching
    between terminal emulators.

--- modules/fish/functions/fish_prompt.fish
+++ modules/fish/functions/fish_prompt.fish
@@ -2,34 +2,28 @@ function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
     set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
   
-    if not set -q __fish_git_prompt_show_informative_status
-        set -g __fish_git_prompt_show_informative_status 1
-    end
-    if not set -q __fish_git_prompt_hide_untrackedfiles
-        set -g __fish_git_prompt_hide_untrackedfiles 1
-    end
-    if not set -q __fish_git_prompt_color_branch
-        set -g __fish_git_prompt_color_branch green --bold
-    end
-    if not set -q __fish_git_prompt_showupstream
-        set -g __fish_git_prompt_showupstream informative
-    end
-    if not set -q __fish_git_prompt_color_dirtystate
-        set -g __fish_git_prompt_color_dirtystate blue
-    end
-    if not set -q __fish_git_prompt_color_stagedstate
-        set -g __fish_git_prompt_color_stagedstate yellow
-    end
-    if not set -q __fish_git_prompt_color_invalidstate
-        set -g __fish_git_prompt_color_invalidstate red
-    end
-    if not set -q __fish_git_prompt_color_untrackedfiles
-        set -g __fish_git_prompt_color_untrackedfiles $fish_color_normal
-    end
-    if not set -q __fish_git_prompt_color_cleanstate
-        set -g __fish_git_prompt_color_cleanstate green --bold
-    end
-  
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_show_informative_status 1
+    set -g __fish_git_prompt_use_informative_chars 0
+    set -g __fish_git_prompt_showupstream informative
+    set -g __fish_git_prompt_hide_untrackedfiles 1
+
+    set -g __fish_git_prompt_char_stateseparator " "
+    set -g __fish_git_prompt_char_dirtystate "*"
+    set -g __fish_git_prompt_char_invalidstate "#"
+    set -g __fish_git_prompt_char_stagedstate "+"
+    set -g __fish_git_prompt_char_stashstate "\$"
+    set -g __fish_git_prompt_char_untrackedfiles "…"
+    set -g __fish_git_prompt_char_upstream_ahead ">"
+    set -g __fish_git_prompt_char_upstream_behind "<"
+    set -g __fish_git_prompt_char_upstream_diverged "<>"
+
+    set -g __fish_git_prompt_color_branch green --bold
+    set -g __fish_git_prompt_color_dirtystate blue
+    set -g __fish_git_prompt_color_stagedstate yellow
+    set -g __fish_git_prompt_color_invalidstate red
+    set -g __fish_git_prompt_color_cleanstate green --bold
+
     set -l color_cwd
     set -l suffix
     if functions -q fish_is_root_user; and fish_is_root_user
@@ -49,7 +43,7 @@ function fish_prompt --description 'Write out the prompt'
     echo -n (prompt_pwd)
     set_color normal
   
-    printf '%s ' (fish_vcs_prompt)
+    printf '%s ' (fish_git_prompt)
   
     set -l status_color (set_color $fish_color_status)
     set -l statusb_color (set_color --bold $fish_color_status)
