b6f574dbf61cd55872a5f8e776c88a7bc7005ecc
Johannes Maier <johannes.maier@mailbox.org>
2025-08-26 23:05:03 +0200

    neovim/snippets: fix language identifier

--- modules/neovim/nvim/lua/snippets/elixir.lua
+++ modules/neovim/nvim/lua/snippets/elixir.lua
@@ -5,7 +5,7 @@ local s = luasnip.s
 local t = luasnip.t
 local i = luasnip.i
 
-luasnip.add_snippets("go", {
+luasnip.add_snippets("elixir", {
   s("m", {
     t "defmodule ",
     i(1, "TheModule"),
