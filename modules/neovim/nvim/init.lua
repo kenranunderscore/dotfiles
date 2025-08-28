85e8094ee82890194dbbb43c46496335689d102f
Johannes Maier <johannes.maier@mailbox.org>
2025-08-28 10:51:42 +0200

    neovim/neovide: configure

--- modules/neovim/nvim/init.lua
+++ modules/neovim/nvim/init.lua
@@ -2,3 +2,7 @@ vim.loader.enable()
 require "config.options"
 require "config.keymap"
 require "config.lazy"
+
+if vim.g.neovide then
+  require "config.neovide"
+end
---
+++ modules/neovim/nvim/lua/config/neovide.lua
@@ -0,0 +1,4 @@
+vim.o.guifont = "TX-02:h16"
+vim.g.neovide_cursor_vfx_mode = "railgun"
+vim.g.neovide_cursor_vfx_particle_lifetime = 1.0
+vim.g.neovide_cursor_vfx_particle_density = 1.0
