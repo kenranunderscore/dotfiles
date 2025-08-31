ce6f628c668c7a77ef738b0570bd6a42227cb40b
Johannes Maier <johannes.maier@mailbox.org>
2025-08-31 21:56:25 +0200

    neovim: telescope -> fzf.lua
    Since I'm using `fzf` in the terminal for many things, I welcome the
    possibility to have the exact same filtering behaviour in Neovim. Bonus:
    I really like the builtins over the ones I've tried in Telescope. Surely
    both can be configured to satisfaction, but it's easier this way.

---
+++ modules/neovim/nvim/lua/plugins/fzf.lua
@@ -0,0 +1,52 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  cmd = "FzfLua",
+  config = function()
+    local small_window = {
+      width = 0.5,
+    }
+    require("fzf-lua").setup {
+      fzf_opts = {
+        -- Override some values that are otherwise inherited from
+        -- my shell's $FZF_DEFAULT_OPTS
+        ["--select-1"] = false,
+        ["--exit-0"] = false,
+        ["--layout"] = "default",
+      },
+      winopts = {
+        height = 0.4,
+        row = 0.5,
+        col = 0.5,
+      },
+      files = {
+        previewer = false,
+        winopts = small_window,
+      },
+      oldfiles = {
+        previewer = false,
+        winopts = small_window,
+      },
+      buffers = {
+        previewer = false,
+        winopts = small_window,
+      },
+      grep = {
+        winopts = {
+          width = 0.9,
+          height = 0.6,
+          row = 0.5,
+          col = 0.5,
+          preview = {
+            horizontal = "right:40%",
+          },
+        },
+      },
+    }
+  end,
+  keys = {
+    { "<leader>pf", "<cmd>FzfLua files<cr>", desc = "Find file" },
+    { "<leader>b", "<cmd>FzfLua buffers<cr>", desc = "Select buffer" },
+    { "<leader>ps", "<cmd>FzfLua live_grep_native<cr>", desc = "Find file" },
+  },
+}
