09d644033b95ac8f7f996c4c5346b9fe7d3b568f
Johannes Maier <johannes.maier@mailbox.org>
2025-09-26 15:40:46 +0200

    neovim: zap mini.hues

--- modules/neovim/nvim/lua/plugins/themes.lua
+++ modules/neovim/nvim/lua/plugins/themes.lua
@@ -4,7 +4,6 @@ return {
   { "shaunsingh/solarized.nvim", lazy = false, event = "VeryLazy" },
   { "eldritch-theme/eldritch.nvim", lazy = false, event = "VeryLazy", opts = {} },
   { "ptdewey/darkearth-nvim", lazy = false, event = "VeryLazy" },
-  { "nvim-mini/mini.hues", lazy = false, event = "VeryLazy" },
   {
     "bluz71/vim-moonfly-colors",
     lazy = false,
