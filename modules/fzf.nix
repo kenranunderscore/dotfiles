5ef2a0d3589269d1d16d97e3568a96859992c283
Johannes Maier <johannes.maier@mailbox.org>
2025-10-09 14:36:57 +0200

    fzf: use C-p/n for scrolling, M-p/n for history

--- modules/fzf.nix
+++ modules/fzf.nix
@@ -12,7 +12,7 @@
     home = {
       packages = [ pkgs.fzf ];
       sessionVariables = {
-        FZF_DEFAULT_OPTS = "--exact --extended --border sharp --height 40% --layout reverse --select-1 --exit-0 --preview-window ',border-sharp'";
+        FZF_DEFAULT_OPTS = "--exact  --extended  --select-1  --exit-0  --bind ctrl-p:up,ctrl-n:down,alt-p:prev-history,alt-n:next-history --layout reverse  --height 40%  --border sharp  --preview-window ',border-sharp'";
         FZF_ALT_C_OPTS = "--preview 'tree -C {} | head -n 50'";
         FZF_CTRL_T_OPTS = "--prompt 'Choose file: '";
         FZF_TMUX = "1";
