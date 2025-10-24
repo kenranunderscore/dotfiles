87f46b0f320a671520024a62de6fae455ef8ab8e
Johannes Maier <johannes.maier@mailbox.org>
2025-10-24 21:54:38 +0200

    kakoune: install kak-bundle automatically with home-manager

--- modules/kakoune/default.nix
+++ modules/kakoune/default.nix
@@ -17,21 +17,33 @@
       }
     ];
 
-    home.packages =
-      let
-        k = pkgs.writeShellScriptBin "k" ''
-          if [ -z "$TMUX" ]; then
-            exec tmux new -As kak kak "$@"
-          else
-            exec kak "$@"
-          fi
-        '';
-      in
-      [
-        k
-        pkgs.kakoune
-        pkgs.kak-lsp
-        pkgs.kak-tree-sitter
-      ];
+    home = {
+      activation.installKakounePlugins = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+        $DRY_RUN_CMD mkdir -p "$XDG_CONFIG_HOME/kak/bundle"
+        dest="$XDG_CONFIG_HOME/kak/bundle/kak-bundle"
+        if [ ! -d "$dest" ]; then
+          $DRY_RUN_CMD \
+            ${lib.getExe pkgs.gitMinimal} clone \
+            https://github.com/jdugan6240/kak-bundle \
+            "$dest"
+        fi
+      '';
+      packages =
+        let
+          k = pkgs.writeShellScriptBin "k" ''
+            if [ -z "$TMUX" ]; then
+              exec tmux new -As kak kak "$@"
+            else
+              exec kak "$@"
+            fi
+          '';
+        in
+        [
+          k
+          pkgs.kakoune
+          pkgs.kak-lsp
+          pkgs.kak-tree-sitter
+        ];
+    };
   };
 }
